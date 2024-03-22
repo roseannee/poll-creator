0e148ac28d6010896f463503690741d17b34c9fe
Daria <roseannee.kim@gmail.com>
2024-03-22 17:23:19 +0100

    feat: add refine method to zod schema

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,14 +1,19 @@
 import { z } from "zod"
 
+// TODO uniqunes + validation
 export const PollSchema = z.object({
   question: z.string().min(1, {
     message: "Question is required.",
   }),
-  options: z.array(
-    z.string().min(1, {
-      message: "Option is required.",
-    })
-  ),
+  options: z
+    .array(
+      z.string().min(1, {
+        message: "Option is required.",
+      })
+    )
+    .refine((items) => new Set(items).size === items.length, {
+      message: "Must be an array of unique strings",
+    }),
 })
 
 export const VoteSchema = z.object({
