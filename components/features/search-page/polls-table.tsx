118aa976a1fcd9d90e6845f2098b61057e9c0a36
Daria <roseannee.kim@gmail.com>
2024-03-26 17:26:31 +0100

    feat(search table): add not found message

--- components/features/search-page/polls-table.tsx
+++ components/features/search-page/polls-table.tsx
@@ -1,11 +1,12 @@
 import { fetchFilteredPolls } from "@/lib/data"
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table"
+import { Icons } from "@/components/shared/icons"
+import Typography from "@/components/typography"
 
 import { TableActions } from "./table-actions"
 import { TableBadge } from "./table-badge"
 import { MyTableHeader } from "./table-header"
 
-// TODO add not found message
 export async function PollsTable({
   query,
   currentPage,
@@ -20,22 +21,38 @@ export async function PollsTable({
       <MyTableHeader />
 
       <TableBody>
-        {polls.map(({ id, question, _count }) => (
-          <TableRow key={id}>
-            <TableCell className="max-w-10 truncate font-medium">
-              {question}
-            </TableCell>
-            <TableCell>
-              <TableBadge count={_count.options} item="option" />
-            </TableCell>
-            <TableCell>
-              <TableBadge count={_count.votes} item="vote" />
-            </TableCell>
-            <TableCell className="text-right">
-              <TableActions pollId={id} />
+        {polls.length === 0 ? (
+          <TableRow>
+            <TableCell colSpan={4}>
+              <div className="flex flex-col items-center justify-center space-y-2">
+                <Icons.frown />
+                <Typography
+                  affects="removePMargin"
+                  className="italic text-muted-foreground"
+                >
+                  No polls found...
+                </Typography>
+              </div>
             </TableCell>
           </TableRow>
-        ))}
+        ) : (
+          polls.map(({ id, question, _count }) => (
+            <TableRow key={id}>
+              <TableCell className="max-w-10 truncate font-medium">
+                {question}
+              </TableCell>
+              <TableCell>
+                <TableBadge count={_count.options} item="option" />
+              </TableCell>
+              <TableCell>
+                <TableBadge count={_count.votes} item="vote" />
+              </TableCell>
+              <TableCell className="text-right">
+                <TableActions pollId={id} />
+              </TableCell>
+            </TableRow>
+          ))
+        )}
       </TableBody>
     </Table>
   )
