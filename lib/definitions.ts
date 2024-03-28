67d1f80b0a69c5f13e6341939f187f92982d4e13
Daria <roseannee.kim@gmail.com>
2024-03-28 16:34:52 +0100

    fix(schema): update error messages

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,19 +1,29 @@
 import { z } from "zod"
 
 export const PollSchema = z.object({
-  question: z.string().refine((value) => value.trim().length > 0, {
-    message: "Question is required.",
-  }),
+  question: z
+    .string()
+    .min(1, { message: "Question is required." })
+    .refine((value) => value.trim().length > 0, {
+      message: "Question cannot be an empty field.",
+    }),
   options: z
-  .array(
-    z.string().refine((value) => value.trim().length > 0, {
-      message: "Option is required.",
-    })
+    .array(
+      z
+        .string()
+        .min(1, { message: "Option is required." })
+        .refine((value) => value.trim().length > 0, {
+          message: "Option cannot be an empty field.",
+        })
     )
     // TODO show an error
-    .refine((items) => new Set(items.map((item) => item.trim())).size === items.length, {
-      message: "Must be an array of unique strings",
-    }),
+    .refine(
+      (items) =>
+        new Set(items.map((item) => item.trim())).size === items.length,
+      {
+        message: "Must be an array of unique strings",
+      }
+    ),
 })
 
 export const VoteSchema = z.object({
